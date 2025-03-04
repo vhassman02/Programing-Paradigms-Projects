9f08655887bc69b3ada99bc1686ef4304b5e9d94
Vincent H. <vhassman02@yahoo.com>
2025-03-03 21:28:20 -0600

    Create build.bat

---
+++ Java/Non-Polymorphic/build.bat
@@ -0,0 +1,10 @@
+::@echo off
+cls
+javac Game.java View.java Controller.java Model.java Tile.java Link.java Json.java
+if %errorlevel% neq 0 (
+	echo There was an error; exiting now.	
+) else (
+	echo Compiled correctly!  Running Game...
+	java Game	
+)
+
